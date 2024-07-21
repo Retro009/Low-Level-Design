729c71db63a825165e31e1c1310baa6710b592b2
user-0 <jeeshannick@gmail.com>
2024-07-21 10:13:02 +0000

    This is a system-generated commit

--- src/main/java/com/scaler/controllers/MenuController.java
+++ src/main/java/com/scaler/controllers/MenuController.java
@@ -1,12 +1,14 @@
 package com.scaler.controllers;
 
 import com.scaler.dtos.*;
+import com.scaler.models.MenuItem;
 import com.scaler.services.MenuService;
 
+import java.util.List;
 
 public class MenuController {
 
-    private final MenuService menuService;
+    private MenuService menuService;
 
     public MenuController(MenuService menuService) {
         this.menuService = menuService;
